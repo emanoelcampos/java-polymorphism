7583ea5ac640d7c73387acaf8af07873891e6cc3
emanoelcampos <emanoel.cv@gmail.com>
2023-06-17 00:34:25 -0300

    abstract classes and methods

---
+++ src/module5/abstractclasses/methods/bank/account/CurrentAccount.java
@@ -0,0 +1,20 @@
+package module5.abstractclasses.methods.bank.account;
+
+public class CurrentAccount extends Account {
+
+    public CurrentAccount(int branchNumber, int accountNumber) {
+        super(branchNumber, accountNumber);
+    }
+
+    @Override
+    public void depositFunds(double amount) {
+       super.balance =+ amount;
+    }
+
+    @Override
+    public boolean withdrawFunds(double amount) {
+        //double withdrawFundsWithTax = amount + 0.2;
+        amount += 0.2;
+        return super.withdrawFunds(amount);
+    }
+}
